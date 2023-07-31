28435d1dd2b6cd9f9b549de4cfdca9b55b943f42
Nata Kuznetsova <natanavky@icloud.com>
2023-07-31 15:31:11 +0300

    Lesson02

---
+++ MyMaps/MyMaps/MapViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewModel.swift
+//  MyMaps
+//
+//  Created by Nata Kuznetsova on 31.07.2023.
+//
+
+import Foundation
+
+final class MapViewModel {
+
+    let value: Int
+    init(value: Int) {
+        self.value = value
+    }
+}
